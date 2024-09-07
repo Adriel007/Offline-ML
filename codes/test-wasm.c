12aeafd0797336b0dca6dd7c01ede28ce84d6a95
Guilherme Santana Dória <148827788+gsdoria@users.noreply.github.com>
2024-09-07 20:25:38 -0300

    Update test-wasm.c

--- codes/test-wasm.c
+++ codes/test-wasm.c
@@ -1,28 +1,43 @@
 #include <math.h>
 #include <time.h>
-#include <sys/time.h>
+#include <stdio.h>
+#include <stdint.h>
 
-#define NUM_ITERATIONS 100000000
+#define NUM_ITERATIONS 10000000
+#define AMOUNT 10
 
-double microtime(int usec)
-{
-    struct timeval tv;
-    gettimeofday(&tv, NULL);
-    return (double)tv.tv_sec * 1000.0 + (double)tv.tv_usec / 1000.0;
+double microtime() {
+    time_t t;
+    struct tm* tm_info;
+
+    time(&t);
+    tm_info = gmtime(&t);
+
+    double epochMillis = (t);
+    return epochMillis;
 }
 
-double compute()
-{
+double compute() {
     double sum = 0.0;
-    for (int i = 0; i < NUM_ITERATIONS; i++)
-    {
-        sum += (sin(i) * cos(i)) / (microtime(0) * 1000);
+    for (int i = 0; i < NUM_ITERATIONS; i++) {
+        sum += (sin(i) * cos(i)) / (microtime() * 1000);
     }
     return sum;
 }
 
-int main()
-{
-    compute();
+int main() {
+    printf("inicio\n");
+    double media = 0;
+    for(int i=0; i<AMOUNT; i++){
+        clock_t start = clock();
+        double result = compute();
+        clock_t end = clock();
+        double time_spent = (double)(end - start) / CLOCKS_PER_SEC;
+        media += time_spent;
+        printf("Time spent %i: %f seconds\n", i+1, time_spent);
+    }
+    printf("Average: %f\n", media/AMOUNT);
+    printf("Total: %f\n", media);
+    printf("fim\n");
     return 0;
 }
