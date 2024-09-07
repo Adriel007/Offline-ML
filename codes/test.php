0c1dd91a06bb156b76428a43983f2af6fa97c861
Guilherme Santana Dória <148827788+gsdoria@users.noreply.github.com>
2024-09-07 20:22:30 -0300

    Update test.php

--- codes/test.php
+++ codes/test.php
@@ -1,6 +1,6 @@
-<?php
-function compute()
-{
+<?php set_time_limit(900);
+
+function compute() {
     $sum = 0.0;
     for ($i = 0; $i < 100000000; $i++) {
         $sum += (sin($i) * cos($i)) / (microtime(true) * 1000);
@@ -8,11 +8,20 @@ function compute()
     return $sum;
 }
 
-$start = microtime(true);
-$result = compute();
-$end = microtime(true);
+$media = 0;
 
-$time_spent = $end - $start;
-echo "Time spent: $time_spent seconds\n";
+for($i = 0; $i<10; $i++){
+    $start = microtime(true);
+    $result = compute();
+    $end = microtime(true);
+    $dt = $end - $start; echo "Time spent ".($i+1).": $dt seconds<br>";
+    $media += $dt;
+}
 
-exit;
+$media /= 10;
+
+echo "<br>media: $media<br>";
+$media*=10;
+echo "total: $media";
+
+exit;?>
